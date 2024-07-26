58630abe01c825eea74f4cf8bff6e00fb469f8ac
Justtin Hoang <daminhjusttinhoang11@gmail.com>
2024-07-26 09:31:22 -0400

    add basepath for prod

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/MyPage' : '',
     output: 'export',
     distDir: 'dist',
     images:{
