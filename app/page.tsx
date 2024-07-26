0904ec9d41e94df91c3973e1ebce6702049dda02
Justtin Hoang <daminhjusttinhoang11@gmail.com>
2024-07-26 07:30:30 -0400

    added links

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    
     <main className="">
+      <title>Justtin Hoang</title>
          <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300 text-black p-4">
     <div className="flex flex-col md:flex-row items-center gap-5 max-w-screen-lg w-full">
       <div>
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
           Hey! I'm <span className="text-blue-600 dark:text-blue-500">Justtin Hoang</span>
         </h1>
+        <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-black">“You can’t add more days to your life but you can add more life to your days.”</p>
         <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-black">
           I am a full-stack developer who loves to create. I enjoy many aspects of life like figuring out how to get around a foreign country, finding the perfect ratios of making the perfect coffee, or making life-lasting connections with strangers. I just graduated from Toronto Metropolitan University and am now seeking a full-time position.
         </p>
         <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-black">
           I am experienced working in diverse work cultures from working with startups to large corporations. My go-to technologies are React and Django. I am capable of picking up new technologies for new projects. I believe that software development is reliant on the developer’s mind.
         </p>
-        <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-black">Feel free to connect with me on LinkedIn, or email me here!</p>
-        <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-black">“You can’t add more days to your life but you can add more life to your days.”</p>
+        <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-black">
+        Feel free to connect with me on <a href="https://www.linkedin.com/in/justtin-hoang/" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">LinkedIn</a>, or email me <a href="mailto:dmj.hoang@gmail.com" className="text-blue-500 underline">here</a>!
+      </p>
       </div>
       <div className="rounded-lg overflow-hidden">
         <Image 
@@ -30,9 +34,15 @@ export default function Home() {
       </div>
     </div>
     <div className="mt-5">
-      <button className="mr-2">GitHub</button>
-      <button className="mr-2">Resume</button>
-      <button>LinkedIn</button>
+      <a href="https://github.com/jhoangy" target="_blank" rel="noopener noreferrer">
+          <button className="mr-2">GitHub</button>
+        </a>
+        <a href="https://drive.google.com/file/d/1TTL4gHCYNdYJ01RIwrUPxUTkT0_cIGZX/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+          <button className="mr-2">Resume</button>
+        </a>
+        <a href="https://www.linkedin.com/in/justtin-hoang/" target="_blank" rel="noopener noreferrer">
+          <button>LinkedIn</button>
+        </a>
     </div>
   </div>
 		
