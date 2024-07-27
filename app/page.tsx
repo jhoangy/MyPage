9c4196f280ee4eef2e775c24a12cc1f634688935
Justtin Hoang <daminhjusttinhoang11@gmail.com>
2024-07-27 01:12:31 -0400

    heading size change

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300 text-black p-4">
       <div className="flex flex-col md:flex-row items-center gap-5 max-w-screen-lg w-full">
         <div>
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
+          <h2 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
             Hey! I&apos;m <span className="text-blue-600 dark:text-blue-500">Justtin Hoang</span>
-          </h1>
+          </h2>
           <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-black">“You can’t add more days to your life but you can add more life to your days.”</p>
           <p className="mb-3 text-lg text-gray-500 md:text-xl dark:text-black">
             I am a full-stack developer who loves to create. I enjoy many aspects of life like figuring out how to get around a foreign country, finding the perfect ratios of making the perfect coffee, or making life-lasting connections with strangers. I just graduated from Toronto Metropolitan University and am now seeking a full-time position.
